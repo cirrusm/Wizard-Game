5114c816477cac31ad91f6295e27b3d34b42c7f1
Cirrus Mokhtari <Cirrus1994@gmail.com>
2021-01-03 19:22:17 -0800

    player can now battle the enemy

--- main.py
+++ main.py
@@ -2,7 +2,30 @@ from classes.game import Person, bcolors
 
 magic = [{"name": "Fire", "cost": 10, "dmg": 60},
         {"name": "Thunder", "cost": 10, "dmg": 80},
-        {"name": "Blizzard", "cost": 10, "dmg": 60}]
-player = Person(460, 65, 60, 34, magic)
+        {"name": "Blizzard", "cost": 12, "dmg": 60}]
 
-print(player.generate_spell_damage(0))
+player = Person(460, 65, 60, 34, magic)
+enemy = Person(1200, 65, 45, 25, magic)
+
+running = True
+i=0
+print(bcolors.FAIL + bcolors.BOLD + "AN ENEMY ATTACKS" + bcolors.ENDC)
+
+while running:
+    print("==============================")
+    player.choose_action()
+    choice = input("Choose action:")
+    index = int(choice) - 1
+
+    if index == 0:
+        dmg = player.generate_damage()
+        enemy.take_damage(dmg)
+        print(f"You did {dmg} to the Enemy. Enemy HP: {enemy.get_hp()}")
+
+    enemy_choice = 1
+
+    enemy_dmg = enemy.generate_damage()
+    player.take_damage(enemy_dmg)
+    print(f"Enemy attacks for {enemy_dmg} Player HP: {player.get_hp()}")
+
+    running = False
